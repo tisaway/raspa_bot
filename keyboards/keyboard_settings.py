a1f19b1f9b70274422652795ef00e31c356240e9
Александра Торопова <39136820+tisaway@users.noreply.github.com>
2025-05-14 16:34:49 +0300

    future feature

--- keyboards/keyboard_settings.py
+++ keyboards/keyboard_settings.py
@@ -1,7 +1,8 @@
 from vkbottle import Keyboard, KeyboardButtonColor, Text
 
 KEYBOARD_SETTINGS = Keyboard(one_time=False, inline=False)
-KEYBOARD_SETTINGS.add(Text("❌ Отписаться от рассылки"), color=KeyboardButtonColor.SECONDARY)
+# Если планируются рассылки
+# KEYBOARD_SETTINGS.add(Text("❌ Отписаться от рассылки"), color=KeyboardButtonColor.SECONDARY)
 KEYBOARD_SETTINGS.add(Text("⚙️ Изменить группу", payload={"cmd":"change_group"}), color=KeyboardButtonColor.SECONDARY)
 KEYBOARD_SETTINGS.row()
 KEYBOARD_SETTINGS.add(Text("⬅️ Назад", payload={"cmd":'menu'}), color=KeyboardButtonColor.SECONDARY)
